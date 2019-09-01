f725264cf37c915c08c143ec1809b63640b0d158
vengatesanns <vengat19amd@gmail.com>
2019-09-01 23:06:49 +0530

    # Properties from Authorization Server to YML File

--- AppSecurity/src/main/java/com/hackpro/appsecurity/config/AuthorizationServer.java
+++ AppSecurity/src/main/java/com/hackpro/appsecurity/config/AuthorizationServer.java
@@ -1,6 +1,7 @@
 package com.hackpro.appsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,8 +20,31 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	// Secret Id -> Hack-Pro-Secret
+
+	@Value("${security.oauth2.client.id}")
+	private String CLIENT_ID;
+
+	@Value("${security.oauth2.client.client-secret}")
+	private String CLIENT_SECRET_ID;
+
+	@Value("${security.oauth2.client.authorized-grant-types}")
+	private String[] CLIENT_AUTHORIZED_TYPES;
+
+	@Value("${security.oauth2.client.authorities}")
+	private String[] AUTHORITIES;
+
+	@Value("${security.oauth2.client.scope}")
+	private String[] SCOPE;
+
+	@Value("${security.oauth2.client.resource-ids}")
+	private String[] RESOURCE_IDS;
+
+	@Value("${security.oauth2.client.access-token-validity-seconds}")
+	private int ACCESS_TOKEN_VALIDITY_SECONDS;
+
+	@Value("${security.oauth2.client.refresh-token-validity-seconds}")
+	private int REFRESH_TOKEN_VALIDITY_SECONDS;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -29,10 +53,10 @@ public class AuthorizationServer extends AuthorizationServerConfigurerAdapter {
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("hackpro_clients").authorizedGrantTypes("client_credentials", "password","refresh_token")
-				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust")
-				.resourceIds("oauth2-resource").accessTokenValiditySeconds(30)
-				.secret(passwordEncoder.encode("secret"));
+		clients.inMemory().withClient(CLIENT_ID).secret(CLIENT_SECRET_ID).authorizedGrantTypes(CLIENT_AUTHORIZED_TYPES)
+				.authorities(AUTHORITIES).scopes(SCOPE).resourceIds(RESOURCE_IDS)
+				.accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
+				.refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY_SECONDS);
 	}
 
 	@Override
